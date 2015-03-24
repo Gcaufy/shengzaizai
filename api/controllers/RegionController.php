9d843cda38c31203f9bf3e2f264843a390d26bf6
gcaufy <gcaufy@gmail.com>
2015-03-24 23:26:01 +0800

    region fix

--- api/controllers/RegionController.php
+++ api/controllers/RegionController.php
@@ -21,5 +21,14 @@ class RegionController extends BaseController
         unset($actions['update'], $actions['delete'], $actions['create']);
         return $actions;
     }
+    protected function getQuery() {
+        $parent_id = Yii::$app->request->get('parent_id');
+        $query = parent::getQuery();
+        if (!$parent_id)
+            $query = $query->andWhere('t.parent_id is null');
+        else
+            $query = $query->andWhere(['t.parent_id' => $parent_id]);
+        return $query;
+    }
 
 }
